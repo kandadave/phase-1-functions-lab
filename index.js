b1dd461bcb2012de44df0a01de5e296092463806
kandadave <kandachumo@gmail.com>
2025-06-06 12:29:15 +0300

    define js functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const HQ_STREET = 42;
+    return Math.abs(street - HQ_STREET);
+}
+
+function distanceFromHqInFeet(street) {
+    const FEET_PER_BLOCK = 264;
+    return distanceFromHqInBlocks(street) * FEET_PER_BLOCK;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const FEET_PER_BLOCK = 264;
+    return Math.abs(start - destination) * FEET_PER_BLOCK;
+}
+
+function calculatesFarePrice(start, destination) {
+    const totalFeetTravelled = distanceTravelledInFeet(start, destination);
+
+    if (totalFeetTravelled <= 400) {
+        return 0; 
+    }
+    else if (totalFeetTravelled > 2500) {
+        return 'cannot travel that far'; 
+    }
+    else if (totalFeetTravelled > 400 && totalFeetTravelled <= 2000) {
+        const chargeableFeet = totalFeetTravelled - 400; 
+        const fareInCents = chargeableFeet * 2; 
+        return fareInCents / 100; 
+    }
+    
+    else if (totalFeetTravelled > 2000) {
+        return 25; 
+    }
+    return 'Error: Could not calculate fare.';
+}
+
+
